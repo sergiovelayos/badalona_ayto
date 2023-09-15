3d1f5ff84711e87eb69cb811c5a70da164ea082b
velayos <sergiovelayos@gmail.com>
2023-09-15 18:44:53 +0200

    update

--- download_all_pdf.py
+++ download_all_pdf.py
@@ -1,20 +1,46 @@
-# Source: https://stackoverflow.com/questions/54616638/download-all-pdf-files-from-a-website-using-python
-import os
+# Source: https://www.youtube.com/watch?v=ng2o98k983k
 import requests
-from urllib.parse import urljoin
 from bs4 import BeautifulSoup
+import urllib.request
+import os
+import re 
 
-url = "http://badalona.cat/portalWeb/badalona.portal;jsessionid=2JV7kDpNdyhPCvrZCXQ9mhPcLvFSY2ywxHhy2lvv17YGnH8Hs7xR!232281408?_nfpb=true&_pageLabel=contingut_estatic&dCollectionID=4116#wlp_contingut_estatic"
 
+# Definir la URL de la página web
+url = "http://badalona.cat/portalWeb/badalona.portal;jsessionid=4ylNk92VwLgsvf93SfNv9nqJT9Dvpf0kDG4gvCvXzCq27qT01h6n!-862168623?_nfpb=true&_pageLabel=contingut_estatic&dCollectionID=4820#wlp_contingut_estatic"
 
-#If there is no such folder, the script will create one automatically
-folder_location = "/pdfs"
-if not os.path.exists(folder_location):os.mkdir(folder_location)
-
+# make HTTP GET request to the target URL
 response = requests.get(url)
-soup= BeautifulSoup(response.text, "html.parser")     
-for link in soup.select("a[href$='.pdf']"):
-    #Name the pdf files using the last portion of each link which are unique in this case
-    filename = os.path.join(folder_location,link['href'].split('/')[-1])
-    with open(filename, 'wb') as f:
-        f.write(requests.get(urljoin(url,link['href'])).content)
+
+# parse content
+soup = BeautifulSoup(response.text, 'lxml')
+
+
+# Creo una lista donde añadiré cada url de PDF a descargar
+lista_pdf_urls = []
+
+# Loop que busca las urls de pdfs a descargar
+for noticia in soup.find_all('li', class_='noticia relacionat'):
+    pdf_src = noticia.find('a')['href']
+    pdf_urls = f'http://badalona.cat{pdf_src}'
+    lista_pdf_urls.append(pdf_urls)
+
+# Define the pattern to match
+pattern = "dDocName=(\w+)"
+
+# Loop through the list
+for i in lista_pdf_urls:
+    # Get the response from the URL
+    response = requests.get(i)
+    # Search for the pattern in the URL
+    match = re.search(pattern, i)
+    # If there is a match, get the file name from the group 1
+    if match:
+      file_name = match.group(1)
+      # Open a file with the same name as write mode
+      with open(file_name, "wb") as file:
+        # Write the response content to the file
+        file.write(response.content)
+    
+
+    
